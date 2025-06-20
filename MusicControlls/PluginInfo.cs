8c721611d5e9bd1bed3ea5f7cb48e1c2f759ed47
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-06-20 14:54:06 +0200

    Delete MusicControlls directory

--- MusicControlls/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MusicControls
-{
-	class PluginInfo
-	{
-		public const string GUID = "Graze.MusicControls";
-		public const string Name = "MusicControls";
-		public const string Version = "1.0.2";
-	}
-}

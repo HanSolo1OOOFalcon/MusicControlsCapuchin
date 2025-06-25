151e3d9e9769dfeb2832e4a4724f6cd43001c539
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-06-25 20:54:46 +0200

    Delete ModInfo.cs

--- ModInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MusicControlsCapuchin
-{
-    public class ModInfo
-    {
-        public const string GUID = "hansolo1000falcon.musiccontrolscapuchin";
-        public const string Name = "MusicControlsCapuchin";
-        public const string Version = "1.0.0";
-    }
-}
